e441d94e93276ff149424692a5797213c6b3f2fa
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-01-19 19:51:41 -0500

    initial commit

---
+++ src/test/java/com/mikaelfrancoeur/nondeterministicspelcompilation/NonDeterministicSpelCompilationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.nondeterministicspelcompilation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NonDeterministicSpelCompilationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
